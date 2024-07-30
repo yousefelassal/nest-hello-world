daec3c648c98d624e06bf719b40e352ec8ad971b
Yousef Elassal <yousefsameh24@gmail.com>
2024-07-30 22:09:36 +0300

    generate ping resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PingModule } from './ping/ping.module';
 
 @Module({
-  imports: [],
+  imports: [PingModule],
   controllers: [AppController],
   providers: [AppService],
 })
