302ce455e0e624522e80bad830b995b83820e4c4
Yousef Elassal <yousefsameh24@gmail.com>
2024-07-30 22:39:13 +0300

    generate users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingModule } from './ping/ping.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PingModule],
+  imports: [PingModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
