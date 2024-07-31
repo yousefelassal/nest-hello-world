78393f8ed7d1e3ce8201ac1564239e109899fd44
Yousef Elassal <yousefsameh24@gmail.com>
2024-07-31 19:24:39 +0300

    handle ninjas CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingModule } from './ping/ping.module';
 import { UsersModule } from './users/users.module';
+import { NinjasModule } from './ninjas/ninjas.module';
 
 @Module({
-  imports: [PingModule, UsersModule],
+  imports: [PingModule, UsersModule, NinjasModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { NinjasService } from './ninjas.service';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+
+@Controller('ninjas')
+export class NinjasController {
+  constructor(private readonly ninjasService: NinjasService) {}
+
+  @Get()
+  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    return this.ninjasService.getNinjas(weapon);
+  }
+
+  @Get(':id')
+  getNinja(@Param('id') id: string) {
+    return this.ninjasService.getNinja(+id);
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return this.ninjasService.createNinja(createNinjaDto);
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return this.ninjasService.updateNinja(+id, updateNinjaDto);
+  }
+
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return this.ninjasService.removeNinja(+id);
+  }
+}
